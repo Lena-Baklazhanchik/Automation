c686f7afcafb7502a0a3618f5039bb002b330c69
STESHENOK <lena.baklazhanchik@gmail.com>
2024-09-24 18:40:55 +0300

    Создала метод printThreeWords() который выводит в столбец три слова Orange, Banana, Apple. Создала метод checkSumSign с двумя переменными, сумма которых сравнивается с нулем и выводит сообщение о положительной или отрицательной сумме

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
 public class Main {
+    //1. Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+    }
+    static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
     //2. Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b,
     //и инициализируйте их любыми значениями, которыми захотите. Далее метод должен просуммировать эти переменные,
     //и если их сумма больше или равна 0, то вывести в консоль сообщение “Сумма положительная”, в противном случае - “Сумма отрицательная”;
-    public static void main(String[] args) {
-        checkSumSign();
-    }
     static void checkSumSign() {
         int a;
-        a = 3;
+        a = -23;
         int b;
         b = 10;
         if ((a + b) >= 0) {
