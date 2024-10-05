0a91fb25ee0785f095db1313ff10f7844b0bbb7e
STESHENOK <lena.baklazhanchik@gmail.com>
2024-10-05 20:42:27 +0300

    решение задач по лекции 4 (создание классов, конструкторы класса)

--- src/Person.java
+++ src/Person.java
@@ -21,7 +21,6 @@ public class Person {
     public String tel;
     public double salary;
     public int age;
-    public static int countOld;
 
     public Person(String name, String position, String email, String tel, double salary, int age) {
         this.name = name;
@@ -30,10 +29,6 @@ public class Person {
         this.tel = tel;
         this.salary = salary;
         this.age = age;
-        if (age > 40) {
-            countOld++;
-            printPersonInfo();
-        }
     }
 
 
@@ -50,6 +45,12 @@ public class Person {
         persArray[2] = new Person("Filimonov Daniil", "Developer", "ilovejava@rumbler.net", "892312222", 120000, 32);
         persArray[3] = new Person("Kukaracha Inna", "Sales Manager", "cacheandcookies@gmail.com", "892312333", 35000, 61);
         persArray[4] = new Person("Solyanko Lyutozhor", "Boss", "yalyublyuborshch@mailbox.com", "892312444", 130000, 19);
+
+        for (int i = 0; i < persArray.length; i++) {
+            if (persArray[i].age > 40) {
+                persArray[i].printPersonInfo();
+            }
+        }
 
     }
 }
